b35ead0d4cddde6cbb385b0b9ac20b91dba03786
Andrey Vasenin <vaseninlol@gmail.com>
2017-10-19 20:24:30 +0200

    Add class to store and aggregate statistics data

---
+++ src/main/java/app/Statistics.java
@@ -0,0 +1,180 @@
+package app;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+
+/**
+ * Storage to store and aggregate statistical data.
+ * <p>
+ * Storage expires old values passively. There are several cases when storage
+ * tries to clean up garbage:
+ * <ul>
+ * <li>Storage discovers that user requested an expired key. Specific key will be removed from storage</li>
+ * <li>Expired keys are discovered when report is generated. All expired keys will be removed from storage</li>
+ * <li>New timestamp is added to the storage. Storage scans for all expired values</li>
+ * </ul>
+ * <p>
+ * These guaranties that number of elements in storage <= ttl.
+ * <p>
+ * All operations are thread safe.
+ */
+public class Statistics {
+    final ConcurrentMap<Long, AtomicReference<Record>> storage;
+    final BiPredicate<Long, Long> expiredPred;
+
+    /**
+     * Creates new storage for statistics with fixed time window.
+     *
+     * @param ttl - window time
+     */
+    public Statistics(int ttl) {
+        this((ts, now) -> (ttl <= now - ts));
+    }
+
+    /**
+     * Creates new storage for statistics with custom expiration predicate.
+     * <p>
+     * Predicate accept timestamp of a storage record and current time as first and second
+     * argument respectively. It returns true if record is expired.
+     *
+     * @param pred - expiration predicate
+     */
+    public Statistics(BiPredicate<Long, Long> pred) {
+        this.expiredPred = pred;
+        this.storage = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * Bumps the given value for a specified timestamp.
+     *
+     * @param timestamp epoch time in UTC
+     * @param count     the value to bump
+     * @throws ArithmeticException if the result overflows long
+     */
+    public void bump(long timestamp, long count) {
+        long now = epoch();
+
+        if (now + 1 < timestamp) {
+            throw new IllegalArgumentException(
+                    String.format("Bump for a timestamp from future %d (now: %d)", timestamp, now));
+        }
+
+        if (expiredPred.test(timestamp, now)) {
+            return;
+        }
+
+
+        AtomicReference<Record> newRecord = new AtomicReference<>(new Record(count));
+        AtomicReference<Record> record = storage.putIfAbsent(timestamp, newRecord);
+
+        if (record != null) {
+            record.updateAndGet((x) -> x.bump(count));
+        } else {
+            // new record is added to storage. try to clean up garbage
+            removeOldValue(now);
+        }
+    }
+
+    /**
+     * Returns the statistics for a specified timestamp.
+     * <p>
+     * If value has empty statistics then null is returned.
+     *
+     * @param timestamp epoch time in UTC
+     * @return statistics for a specified timestamp.
+     */
+    public Record get(long timestamp) {
+        if (expiredPred.test(timestamp, epoch())) {
+            storage.remove(timestamp);
+            return null;
+        }
+
+        AtomicReference<Record> ref = storage.get(timestamp);
+        if (null == ref) {
+            return null;
+        }
+        return ref.get();
+    }
+
+    /**
+     * Returns the statistics for all not expired records.
+     * <p>
+     * If value has empty statistics then null is returned.
+     *
+     * @return statistics for a all not expired records.
+     */
+    public Record fullReport() {
+        return reduce(Record::merge);
+    }
+
+    Record reduce(BiFunction<Record, Record, Record> reducer) {
+        long now = epoch();
+        Record acc = null;
+        for (Map.Entry<Long, AtomicReference<Record>> entry : storage.entrySet()) {
+            if (!expiredPred.test(entry.getKey(), now)) {
+                Record r = entry.getValue().get();
+                acc = reducer.apply(acc, r);
+            } else {
+                storage.remove(entry.getKey());
+            }
+        }
+        return acc;
+    }
+
+    private long epoch() {
+        return Instant.now().getEpochSecond();
+    }
+
+    private void removeOldValue(long now) {
+        Set<Long> set = storage.keySet();
+        for (long ts : set) {
+            if (expiredPred.test(ts, now)) {
+                set.remove(ts);
+            }
+        }
+    }
+
+    public static class Record {
+        final long min;
+        final long max;
+        final long sum;
+        final long count;
+
+        Record(long val) {
+            this(val, val, val, 1);
+        }
+
+        Record(long min, long max, long sum, long count) {
+            this.min = min;
+            this.max = max;
+            this.sum = sum;
+            this.count = count;
+        }
+
+        Record bump(long val) {
+            return merge(this, new Record(val));
+        }
+
+        static Record merge(Record a, Record r) {
+            if (null == a) {
+                return r;
+            }
+            if (null == r) {
+                return null;
+            }
+            return new Record(
+                    Math.min(a.min, r.min),
+                    Math.max(a.max, r.max),
+                    Math.addExact(a.sum, r.sum),
+                    Math.addExact(a.count, r.count)
+            );
+        }
+    }
+}
+
---
+++ src/test/java/app/StatisticsTest.java
@@ -0,0 +1,175 @@
+package app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(JUnit4.class)
+public class StatisticsTest {
+    Statistics statistics;
+    long now = Instant.now().getEpochSecond();
+
+    @Test
+    public void validBumpOperationShouldWork() {
+        statistics = new Statistics(10);
+        Statistics.Record record;
+
+        record = statistics.get(now);
+        assertThat(record, is(nullValue()));
+
+        statistics.bump(now, 1);
+
+        record = statistics.get(now);
+        assertThat(record.min, equalTo(1L));
+        assertThat(record.max, equalTo(1L));
+        assertThat(record.sum, equalTo(1L));
+        assertThat(record.count, equalTo(1L));
+
+        statistics.bump(now, 3);
+
+        record = statistics.get(now);
+        assertThat(record.min, equalTo(1L));
+        assertThat(record.max, equalTo(3L));
+        assertThat(record.sum, equalTo(4L));
+        assertThat(record.count, equalTo(2L));
+
+        statistics.bump(now - 1, 4);
+        record = statistics.get(now);
+        assertThat(record.count, equalTo(2L));
+
+        statistics.bump(now + 1, 2);
+        record = statistics.get(now - 1);
+        assertThat(record.count, equalTo(1L));
+
+        record = statistics.fullReport();
+        assertThat(record.min, equalTo(1L));
+        assertThat(record.max, equalTo(4L));
+        assertThat(record.sum, equalTo(10L));
+        assertThat(record.count, equalTo(4L));
+    }
+
+    @Test
+    public void expiresOldWhenUserRequestFullStatistics() {
+        AtomicBoolean seeding = new AtomicBoolean(true);
+        statistics = new Statistics((recordTs, currentTs) -> {
+            if (seeding.get()) {
+                return false;
+            }
+            return recordTs <= now - 2;
+        });
+        statistics.bump(now - 2, 1);
+        statistics.bump(now, 3);
+        statistics.bump(now - 1, 2);
+
+        Statistics.Record record = statistics.fullReport();
+        assertThat(record.min, equalTo(1L));
+        assertThat(record.max, equalTo(3L));
+        assertThat(record.sum, equalTo(6L));
+        assertThat(record.count, equalTo(3L));
+        assertThat(statistics.storage.size(), equalTo(3));
+
+        seeding.set(false);
+        record = statistics.fullReport();
+
+        assertThat(statistics.storage.size(), equalTo(2));
+        assertThat(record.min, equalTo(2L));
+        assertThat(record.max, equalTo(3L));
+        assertThat(record.sum, equalTo(5L));
+        assertThat(record.count, equalTo(2L));
+    }
+
+    @Test
+    public void expiresOldWhenUserAddNewValues() {
+        AtomicBoolean seeding = new AtomicBoolean(true);
+        statistics = new Statistics((recordTs, currentTs) -> {
+            if (seeding.get()) {
+                return false;
+            }
+            return recordTs <= now - 2;
+        });
+
+        statistics.bump(now - 3, 1);
+        statistics.bump(now - 2, 1);
+        statistics.bump(now - 1, 2);
+
+        assertThat(statistics.storage.size(), equalTo(3));
+
+        seeding.set(false);
+
+        // do nothing if we just increase already existing value
+        statistics.bump(now - 1, 2);
+        assertThat(statistics.storage.size(), equalTo(3));
+
+        // scan expired values when we add new one
+        statistics.bump(now, 2);
+        assertThat(statistics.storage.size(), equalTo(2));
+    }
+
+    @Test
+    public void emptyStatsShouldReturnNull() {
+        statistics = new Statistics(10);
+        assertThat(statistics.storage.size(), equalTo(0));
+        assertThat(statistics.fullReport(), is(nullValue()));
+    }
+
+    @Test
+    public void shouldTakeIntoAccountBoundaryValues() {
+        statistics = new Statistics(2);
+        assertThat(statistics.storage.size(), equalTo(0));
+
+        statistics.bump(now, 1);
+        statistics.bump(now - 1, 2);
+        statistics.bump(now - 2, 3);
+
+        assertThat(statistics.storage.size(), is(2));
+        Statistics.Record record = statistics.fullReport();
+        assertThat(record.min, equalTo(1L));
+        assertThat(record.max, equalTo(2L));
+        assertThat(record.count, equalTo(2L));
+    }
+
+    @Test
+    public void randomizedTest() throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+
+        statistics = new Statistics(60);
+        AtomicLong sum = new AtomicLong(0);
+        for (int batchId = 0; batchId < 1000; batchId++) {
+            pool.submit(() -> {
+                        for (int i = 0; i < 10000; i++) {
+                            ThreadLocalRandom localRandom = ThreadLocalRandom.current();
+                            int offset = localRandom.nextInt(10);
+                            int val = localRandom.nextInt(100);
+                            sum.addAndGet(val);
+                            statistics.bump(now - offset, val);
+                        }
+                        statistics.fullReport();
+                    }
+            );
+        }
+        pool.shutdown();
+        pool.awaitTermination(10, TimeUnit.SECONDS);
+
+        Statistics.Record record = statistics.fullReport();
+        assertThat(record.count, equalTo(10000000L));
+        assertThat(record.sum, equalTo(sum.get()));
+        assertThat(statistics.storage.size(), equalTo(10));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void bumpValuesFromFutureShouldFail() {
+        statistics = new Statistics(60);
+        statistics.bump(now + 5, 1);
+    }
+}
